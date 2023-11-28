d15c7cc63e336b2587442d88a4532991cdff36a4
Goblined EZ <113932145+GoblinedEZ@users.noreply.github.com>
2023-11-27 20:20:19 -0600

    lowFilter w/o cutoff frequency added and pins recorded

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,62 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <iostream>
+//min is 0.2 mV, max is 150 mV pre amped, 
+//read 5 times to get average before filtering
 
-// put function declarations here:
-int myFunction(int, int);
+const int IMUpin = 8;
+const int pressPin = 18;
+float filtPress = 0.0;
+float prevFiltPress = 0.0;
+float accel = 0.0;
+float pressure = 0.0;
+float cutoffFrequency = 1.0;
+float delta = 0.2;
+
+float lowFilter(float);    //delta only
+float lowFilter2(float);   //with cutofff frequency??? maybe not applicable
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(IMUpin, INPUT);
+  pinMode(pressPin, INPUT);
+  Wire.setSCL(19);
+  Wire.setSDA(pressPin);
+  Serial.begin(9600);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { // grab values then do it with loops, collect 5 times, then average, then filter
+ 
+
+  for(int i = 0; i < 5, i++;)
+  {
+    float rawAccel = digitalRead(IMUpin);        //read once
+    float rawPressure = analogRead(pressPin);    //read once
+    accel+= rawAccel;
+    pressure += rawPressure;
+  }
+  pressure = pressure/5;                         //find average
+  accel = accel/5;                               //average
+
+  lowFilter(pressure);        //run analog thru
+  Serial.print(accel);        //test output value
+  Serial.print(pressure);     //test output value
+  
+  accel = 0;    //reset
+  pressure = 0; //reset
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float lowFilter(float rawPressure) {         
+  filtPress = (delta*rawPressure)+((1.0-delta)*prevFiltPress);
+  
+  prevFiltPress = filtPress;
+}
+
+float lowFilter2(float rawPressure) {    //not done yet
+  pow(2.718, (1-exp(-delta*2*cutoffFrequency)));
+
+
 }
---
+++ test/Low_Filter_Test.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <wire.h>
+#include <cmath>
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void createACWaveform()
+{
+
+}
+
+void test()
+{
+
+}
